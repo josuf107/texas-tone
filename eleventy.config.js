87539a681938845dd7fde092f7a8d054869d32b6
Your Name <you@example.com>
2025-06-03 22:46:40 -0500

    Set path prefix for gh pages

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,10 @@
+import { HtmlBasePlugin } from "@11ty/eleventy";
+
 export default function(eleventyConfig) {
     eleventyConfig.setInputDirectory("site");
     eleventyConfig.addPassthroughCopy("site/assets/css/*.css");
     eleventyConfig.addPassthroughCopy("site/assets/js/*.js");
     eleventyConfig.addPassthroughCopy("site/assets/webfonts/*");
     eleventyConfig.addPassthroughCopy("site/images/*");
+    eleventyConfig.addPlugin(HtmlBasePlugin);
 };
